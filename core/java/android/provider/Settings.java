57d02ecd5e856b58639402cdbd25e8c1a37b2415
JerryScript <jerryscript@gmail.com>
2012-02-06 04:40:30 -0800

    Add preference for bootanimation chooser
    Change-Id: I1071abefd25c95c234779d8a59e91582471ee8f7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3043,6 +3043,12 @@ public final class Settings {
         public static final String OVERSCROLL_COLOR = "overscroll_color";
 
         /**
+         * Sets the bootanimation pathname for use with CMBootanimation
+         * @hide
+         */
+        public static final String BOOTANIMATION_PATH = "bootanimation_path";
+
+        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
          */
