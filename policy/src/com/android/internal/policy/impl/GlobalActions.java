1731f33c3dadecd8b64d11db20f2c108fd0bedfc
Danny Baumann <dannybaumann@web.de>
2012-03-12 09:36:39 +0100

    Allow theming of framework UI elements.
    As the theme engine works by replacing resources while loading the APK,
    framework UI couldn't be themed so far. Workaround that by manually
    creating a com.android.systemui package context and using that for
    showing the UI. This attempt is a bit hacky, but should be safe as
    a) systemui should always be present
    b) if it isn't, we fall back to no theming
    
    Change-Id: I644e2e8d7d4abf07bae639b606197d691f16adac

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -60,6 +60,8 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.internal.app.ThemeUtils;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -75,6 +77,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private StatusBarManager mStatusBar;
 
     private final Context mContext;
+    private Context mUiContext;
     private final AudioManager mAudioManager;
 
     private ArrayList<Action> mItems;
@@ -168,6 +171,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         filter.addAction(TelephonyIntents.ACTION_EMERGENCY_CALLBACK_MODE_CHANGED);
         context.registerReceiver(mBroadcastReceiver, filter);
 
+       ThemeUtils.registerThemeChangeReceiver(context, mThemeChangeReceiver);
+
         // get notified of phone state changes
         TelephonyManager telephonyManager =
                 (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
@@ -187,6 +192,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     public void showDialog(boolean keyguardShowing, boolean isDeviceProvisioned) {
         mKeyguardShowing = keyguardShowing;
         mDeviceProvisioned = isDeviceProvisioned;
+        if (mDialog != null && mUiContext == null) {
+        mDialog.dismiss();
+        mDialog = null;
+        }
         if (mDialog == null) {
             mStatusBar = (StatusBarManager)mContext.getSystemService(Context.STATUS_BAR_SERVICE);
             mDialog = createDialog();
@@ -195,6 +204,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mStatusBar.disable(StatusBarManager.DISABLE_EXPAND);
         mDialog.show();
+        }
+        private Context getUiContext() {
+            if (mUiContext == null) {
+                mUiContext = ThemeUtils.createUiContext(mContext);
+            }
+            return mUiContext != null ? mUiContext : mContext;
     }
 
     /**
@@ -416,7 +431,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
                 public void onPress() {
                     // shutdown by making sure radio and power are handled accordingly.
-                    ShutdownThread.shutdown(mContext, true);
+                    ShutdownThread.shutdown(getUiContext(),(Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWER_DIALOG_PROMPT, 1) == 1));
                 }
 
                 public boolean showDuringKeyguard() {
@@ -432,7 +448,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         mItems.add(
                 new SinglePressAction(com.android.internal.R.drawable.ic_lock_reboot, R.string.global_action_reboot) {
                     public void onPress() {
-                        ShutdownThread.reboot(mContext, null, (Settings.System.getInt(mContext.getContentResolver(),
+                        ShutdownThread.reboot(getUiContext(), null, (Settings.System.getInt(mContext.getContentResolver(),
                                 Settings.System.POWER_DIALOG_PROMPT, 1) == 1));
                     }
 
@@ -513,7 +529,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        final AlertDialog.Builder ab = new AlertDialog.Builder(mContext);
+        final AlertDialog.Builder ab = new AlertDialog.Builder(getUiContext());
 
         ab.setAdapter(mAdapter, this)
                 .setInverseBackgroundForced(true);
@@ -565,7 +581,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             names[i++] = profile.getName();
         }
 
-        final AlertDialog.Builder ab = new AlertDialog.Builder(mContext);
+        final AlertDialog.Builder ab = new AlertDialog.Builder(getUiContext());
 
         AlertDialog dialog = ab
                 .setSingleChoiceItems(names, checkedItem, new DialogInterface.OnClickListener() {
@@ -708,7 +724,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            final Context context = getUiContext();
+            return action.create(context, convertView, parent, LayoutInflater.from(context));
         }
     }
 
@@ -956,6 +973,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     };
 
+    private BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            mUiContext = null;
+        }
+    };
+
     PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
         @Override
         public void onServiceStateChanged(ServiceState serviceState) {
@@ -991,3 +1014,4 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         mContext.sendBroadcast(intent);
     }
 }
+
