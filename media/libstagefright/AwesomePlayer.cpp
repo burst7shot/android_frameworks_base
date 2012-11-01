steppnasty/platform_frameworks_base

Don't log streaming uri in ChromiumHTTPDataSource. DO NOT MERGE.
o related-to-bug: 6994761

Change-Id: Ic9642edd7b86b4d7543e0f5d397a571976d7e7db

media/libstagefright/AwesomePlayer.cpp
@@ -321,11 +321,7 @@ status_t AwesomePlayer::setDataSource_l(
         }
     }
 
-    if (!(mFlags & INCOGNITO)) {
-        ALOGI("setDataSource_l('%s')", mUri.string());
-    } else {
-        ALOGI("setDataSource_l(URL suppressed)");
-    }
+    ALOGI("setDataSource_l(URL suppressed)");
 
     // The actual work will be done during preparation in the call to
     // ::finishSetDataSource_l to avoid blocking the calling thread in
@@ -2588,7 +2584,7 @@ status_t AwesomePlayer::dump(int fd, const Vector<String16> &args) const {
 
     fprintf(out, " AwesomePlayer\n");
     if (mStats.mFd < 0) {
-        fprintf(out, "  URI(%s)", mStats.mURI.string());
+        fprintf(out, "  <URI suppressed>");
     } else {
         fprintf(out, "  fd(%d)", mStats.mFd);
     }

media/libstagefright/httplive/LiveSession.cpp
@@ -183,11 +183,7 @@ void LiveSession::onConnect(const sp<AMessage> &msg) {
         headers = NULL;
     }
 
-    if (!(mFlags & kFlagIncognito)) {
-        ALOGI("onConnect '%s'", url.c_str());
-    } else {
-        ALOGI("onConnect <URL suppressed>");
-    }
+    ALOGI("onConnect <URL suppressed>");
 
     mMasterURL = url;
 

burst7shot/android_frameworks_base

Conflicts:
	media/libstagefright/AwesomePlayer.cpp
	media/libstagefright/httplive/LiveSession.cpp

media/libstagefright/AwesomePlayer.cpp
@@ -319,11 +319,7 @@ status_t AwesomePlayer::setDataSource_l(
         }
     }
 
-    if (!(mFlags & INCOGNITO)) {
-        LOGI("setDataSource_l('%s')", mUri.string());
-    } else {
-        LOGI("setDataSource_l(URL suppressed)");
-    }
+    LOGI("setDataSource_l(URL suppressed)");
 
     // The actual work will be done during preparation in the call to
     // ::finishSetDataSource_l to avoid blocking the calling thread in
@@ -2498,7 +2494,7 @@ status_t AwesomePlayer::dump(int fd, const Vector<String16> &args) const {
 
     fprintf(out, " AwesomePlayer\n");
     if (mStats.mFd < 0) {
-        fprintf(out, "  URI(%s)", mStats.mURI.string());
+        fprintf(out, "  <URI suppressed>");
     } else {
         fprintf(out, "  fd(%d)", mStats.mFd);
     }

media/libstagefright/httplive/LiveSession.cpp
@@ -174,11 +174,7 @@ void LiveSession::onConnect(const sp<AMessage> &msg) {
         headers = NULL;
     }
 
-    if (!(mFlags & kFlagIncognito)) {
-        LOGI("onConnect '%s'", url.c_str());
-    } else {
-        LOGI("onConnect <URL suppressed>");
-    }
+    LOGI("onConnect <URL suppressed>");
 
     mMasterURL = url;
 